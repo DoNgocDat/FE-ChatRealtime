ad61b4f91ef14d776121820d9df4bb6210244dc9
datdd <ngocdd@gmail.com>
2025-04-04 11:13:01 +0700

    update build in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',  // Chỉ định thư mục build là 'dist'
+  },
   server: {
     proxy: {
       '/auth': 'https://smartchat-fh2a.onrender.com', // Yêu cầu đến /auth sẽ được chuyển tiếp đến localhost:3000
