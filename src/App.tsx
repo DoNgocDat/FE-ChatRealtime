5245e0c5d7538dfd7a596c20de6f137c4a5b3e9f
datdd <ngocdd@gmail.com>
2025-04-05 09:25:25 +0700

    change browserrouter to hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { ThemeProvider } from "./config/theme";
 import AppRouter from "../src/routes/appRouter";
 
 function App() {
   return (
     <ThemeProvider>
-      <BrowserRouter>
+      <HashRouter>
         <AppRouter />
-      </BrowserRouter>
+      </HashRouter>
     </ThemeProvider>
   );
 }
